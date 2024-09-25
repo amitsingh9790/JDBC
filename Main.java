f3d0a90a238c2da05ee5c531b2646086944d55c6
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-09-25 19:06:46 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,35 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+
+        try{
+            // 1. load or register the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Register the driver");
+            //2. Establish Connection
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCDEMO","root","student");
+            System.out.println("Connection Established");
+
+            //3. create statement/PreparedStatment/CollableStatement
+            //ps = con.prepareStatement("CREATE TABLE Student(name VARCHAR(50), rollno INT)");
+            //ps = con.prepareStatement("INSERT INTO student VALUES('Amit',103)");
+            ps = con.prepareStatement("INSERT INTO student VALUES('',)");
+
+            //4. execute the Query
+            int count = ps.executeUpdate(); //return no. of rows affected
+            System.out.println(count);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            ps.close();
+            con.close();
+        }
+    }
+}
