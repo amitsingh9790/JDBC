7b4469f6707b0b1741264279b82748b9b2a17e64
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-09-26 20:59:02 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+package First_JDBC_Code;
+import java.sql.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+//        Scanner sc = new Scanner(System.in);
+//        String name1 = sc.nextLine();
+//        int rollno1 = sc.nextInt();
+
         Connection con = null;
         PreparedStatement ps = null;
 
@@ -12,18 +15,35 @@ public class Main {
             // 1. load or register the driver
             Class.forName("com.mysql.cj.jdbc.Driver");
             System.out.println("Register the driver");
+
             //2. Establish Connection
             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCDEMO","root","student");
             System.out.println("Connection Established");
 
-            //3. create statement/PreparedStatement/CollableStatement
+            //3. create statement/PreparedStatment/CollableStatement
             //ps = con.prepareStatement("CREATE TABLE Student(name VARCHAR(50), rollno INT)");
-            //ps = con.prepareStatement("INSERT INTO student VALUES('Amit',103)");
-            ps = con.prepareStatement("INSERT INTO student VALUES('',)");
-
+            //INSERT
+            ps = con.prepareStatement("INSERT INTO Student value ('Raj',1003)");
+            //UPDATE
+            ps = con.prepareStatement("UPDATE Student SET name = ? Where rollno = ? ");
+            //ps = con.prepareStatement("UPDATE Student SET name = '"+name1+"' WHERE rollno = '"+rollno1+"' ");
+            ps.setString(1,"Amit");
+            ps.setString(2, String.valueOf(1003));
+            
+            //ps = con.prepareStatement("Select * From Student", ResultSet.TYPE_SCROLL_INSENSITIVE,ResultSet.CONCUR_READ_ONLY);
+            
             //4. execute the Query
-            int count = ps.executeUpdate(); //return no. of rows affected
-            System.out.println(count);
+            ps.executeUpdate();
+            //ResultSet rs = ps.executeQuery();
+
+//            while (rs.next()){
+//                String name2 = rs.getString("name");
+//                int rollno2 = rs.getInt("rollno");
+//                System.out.println(name2+" "+rollno2);
+//            }
+            
+            //int count = ps.executeUpdate(); //return no. of rows affected
+            //System.out.println(count);
         }catch (Exception e){
             e.printStackTrace();
         }
