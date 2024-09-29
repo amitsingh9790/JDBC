0ba29ec3396d010f7ca10394b72e91efb8a3ff31
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-09-29 20:22:03 +0530

    Add files via upload

---
+++ First_JDBC_Code/Main2.java
@@ -0,0 +1,41 @@
+package First_JDBC_Code;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class Main2 {
+    public static void main(String[] args) throws SQLException {
+                int rollno = 106;
+                String name = "Rahul";
+
+                Connection con = null;
+                PreparedStatement ps = null;
+                try{
+                    // 1. load or register the driver
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    System.out.println("Driver Registered");
+
+                    // 2. Establish Connection
+                    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCDEMO","root","student");
+                    System.out.println("Connection Established");
+
+                    // 3. create statement/PreparedStatment/CollableStatement
+                    //===================CREATE TABLE===========================
+                    //ps = con.prepareStatement("CREATE TABLE IF NOT EXISTS Student(name VARCHAR(50), rollno INT)");
+
+                    //=======================INSERT==============================
+                    ps = con.prepareStatement("INSERT INTO Student value ('"+name+"','"+rollno+"')");
+
+                    int count = ps.executeUpdate(); //return no. of rows affected
+                    System.out.println(count);
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+                finally {
+                    assert ps != null;
+                    ps.close();
+                    con.close();
+                }
+    }
+}
