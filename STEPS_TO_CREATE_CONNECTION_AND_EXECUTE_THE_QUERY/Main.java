918d26533ec0a9c5b594aff2fd83002372d2b1f6
Amit Singh <94777067+amitsingh9790@users.noreply.github.com>
2024-09-29 20:28:27 +0530

    commit

---
+++ STEPS_TO_CREATE_CONNECTION_AND_EXECUTE_THE_QUERY/Main.java
@@ -0,0 +1,45 @@
+package STEPS_TO_CREATE_CONNECTION_AND_EXECUTE_THE_QUERY;
+import java.sql.*;
+
+public class Main {
+    public static void main(String[] args) throws SQLException {
+        Connection con = null;
+        PreparedStatement ps = null;
+        try{
+            // 1. load or register the driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver Registered");
+
+            // 2. Establish Connection
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/JDBCDEMO","root","student");
+            System.out.println("Connection Established");
+
+            // 3. create statement/PreparedStatment/CollableStatement
+            //===================CREATE TABLE===========================
+            ps = con.prepareStatement("CREATE TABLE IF NOT EXISTS Student(name VARCHAR(50), rollno INT)");
+
+            //=======================INSERT==============================
+            //ps = con.prepareStatement("INSERT INTO Student value (?,?)");
+            //ps.setString(1,"Amit");
+            //ps.setString(2, String.valueOf(1005));
+
+            //=======================UPDATE==============================
+            //ps = con.prepareStatement("UPDATE Student SET name = ? Where rollno = ? ");
+            //ps.setString(1,"Rahul");
+            //ps.setString(2, String.valueOf(1005));
+
+            //========================DELETE=============================
+            //ps = con.prepareStatement("DELETE FROM student WHERE name = 'Amit' ");
+
+            int count = ps.executeUpdate(); //return no. of rows affected
+            System.out.println(count);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        finally {
+            assert ps != null;
+            ps.close();
+            con.close();
+        }
+    }
+}
